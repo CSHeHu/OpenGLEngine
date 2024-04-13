db6977c39081d54db12e768c30f4da24b29b50f2
HeHu <heissendo88@gmail.com>
2024-04-13 15:53:36 +0300

    clean up code

--- OpenGLFramework/src/application.cpp
+++ OpenGLFramework/src/application.cpp
@@ -27,11 +27,6 @@ const unsigned int SCR_HEIGHT = 1080;
 // camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
-/*
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
-bool firstMouse = true;
-*/
 
 // timing
 float deltaTime = 0.0f;	// time between current frame and last frame
@@ -307,32 +302,6 @@ int main()
     return 0;
 }
 
-
-
-/*
-// glfw: whenever the mouse moves, this callback is called
-// -------------------------------------------------------
-void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
-{
-    float xpos = static_cast<float>(xposIn);
-    float ypos = static_cast<float>(yposIn);
-
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-    lastX = xpos;
-    lastY = ypos;
-
-    camera.ProcessMouseMovement(xoffset, yoffset);
-}
-*/
 
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
