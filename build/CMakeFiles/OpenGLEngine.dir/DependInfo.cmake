de049953bc9ebcfc43d3c6a1a962c490534c6c92
Heikki Huttu <heissendo88@gmail.com>
2025-06-10 20:23:29 +0300

    Unclutter tree and add cmake

---
+++ build/CMakeFiles/OpenGLEngine.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/heissendo/dev/OpenGLEngine/src/glad.c" "CMakeFiles/OpenGLEngine.dir/src/glad.c.o" "gcc" "CMakeFiles/OpenGLEngine.dir/src/glad.c.o.d"
+  "/home/heissendo/dev/OpenGLEngine/src/Camera.cpp" "CMakeFiles/OpenGLEngine.dir/src/Camera.cpp.o" "gcc" "CMakeFiles/OpenGLEngine.dir/src/Camera.cpp.o.d"
+  "/home/heissendo/dev/OpenGLEngine/src/InputManager.cpp" "CMakeFiles/OpenGLEngine.dir/src/InputManager.cpp.o" "gcc" "CMakeFiles/OpenGLEngine.dir/src/InputManager.cpp.o.d"
+  "/home/heissendo/dev/OpenGLEngine/src/MeshData.cpp" "CMakeFiles/OpenGLEngine.dir/src/MeshData.cpp.o" "gcc" "CMakeFiles/OpenGLEngine.dir/src/MeshData.cpp.o.d"
+  "/home/heissendo/dev/OpenGLEngine/src/Object.cpp" "CMakeFiles/OpenGLEngine.dir/src/Object.cpp.o" "gcc" "CMakeFiles/OpenGLEngine.dir/src/Object.cpp.o.d"
+  "/home/heissendo/dev/OpenGLEngine/src/Shader.cpp" "CMakeFiles/OpenGLEngine.dir/src/Shader.cpp.o" "gcc" "CMakeFiles/OpenGLEngine.dir/src/Shader.cpp.o.d"
+  "/home/heissendo/dev/OpenGLEngine/src/TextManager.cpp" "CMakeFiles/OpenGLEngine.dir/src/TextManager.cpp.o" "gcc" "CMakeFiles/OpenGLEngine.dir/src/TextManager.cpp.o.d"
+  "/home/heissendo/dev/OpenGLEngine/src/TextureManager.cpp" "CMakeFiles/OpenGLEngine.dir/src/TextureManager.cpp.o" "gcc" "CMakeFiles/OpenGLEngine.dir/src/TextureManager.cpp.o.d"
+  "/home/heissendo/dev/OpenGLEngine/src/application.cpp" "CMakeFiles/OpenGLEngine.dir/src/application.cpp.o" "gcc" "CMakeFiles/OpenGLEngine.dir/src/application.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
